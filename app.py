be42652b5a6a44546cc0cfb339ab7d61596b0e45
InakiGainza <igg.git.h@gmail.com>
2023-03-11 18:46:05 +0100

    add templates

--- app.py
+++ app.py
@@ -2,13 +2,38 @@
 
 from flask import Flask
 from flask import Flask, render_template
+from article_data import Articles
+
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
     #return "<center>Kaixo Team !</center>"
     return render_template('home.html')
+
+
+@app.route('/course')
+def course():
+    return render_template('course.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+
+@app.route('/article/<string:id>/')
+def display_article(id):
+    return render_template('article.html', id=id)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
 
