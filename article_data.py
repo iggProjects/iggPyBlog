a6baf1672abf0ba42238cc241759fa0083d5ebdf
InakiGainza <igg.git.h@gmail.com>
2023-03-13 09:54:29 +0100

    update article_data & articles.html % _navbar

--- article_data.py
+++ article_data.py
@@ -2,24 +2,28 @@ def Articles():
     articles = [       
         {
             'id': 1, 
-            'title': 'First article',
-            'body': 'testing html -- testing',
-            'author': 'Sophia Iroegbu',
-            'date_created': '26-01-2022'            
+            'title': 'First article',            
+            'author': 'Igg',
+            'date_created': '26-01-2022',
+            'summary': 'El Módulo de Caja Flexible, comúnmente llamado flexbox, fue diseñado como un modelo unidimensional de layout, y como un método que pueda ayudar a distribuir el espacio entre los ítems de una interfaz y mejorar las capacidades de alineación. Este artículo hace un repaso de las principales características de flexbox, las que exploraremos con mayor detalle en el resto de estas guías.',
+            'body': 'testing html -- testing'
         },
         {
             'id': 2, 
-            'title': 'Second article',
-            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
-            'author': 'John Doe',
-            'date_created': '25-01-2022'
+            'title': 'Second article',            
+            'author': 'Che',
+            'date_created': '31-01-2022',
+            'summary': 'This defines the ability for a flex item to grow if necessary. It accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up.',
+            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu'
+            
         },
         {
             'id': 3, 
-            'title': 'Third article',
-            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
-            'author': 'Mary James',
-            'date_created': '26-01-2022'
+            'title': 'Third article',            
+            'author': 'Xabier',
+            'date_created': '02-02-2022',
+            'summary': 'Egiaz, Wikipediaren barnean, oraintxe, nahiko arrakastatsuak izaten ari diren zenbait proiektu ikusten ari gara, Europako hizkuntza txikietan. Beharbada, galesez jakitea ez da erabat beharrezkoa izango. Galesez jakinda ingelesez ez dakiten pertsonen kopurua oso txikia da, eta urtero txikiagotuz doa. ',
+            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu'
         },
     ]
 
