d0663c26bf36067e1716a1a4ab82f58b76e92fcb
InakiGainza <igg.git.h@gmail.com>
2023-03-15 11:23:12 +0100

    add simple login  & update bitakora

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # 
 
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, redirect, request
+# LoginManager
+
 from article_data import Articles
 from excercise_data import Excercises
 import os
-
 
 app = Flask(__name__)
 
@@ -32,11 +33,9 @@ def articles():
 def display_article():
     article = 1
     id = int(request.args['id'])
-    #print("len Articles",len(Articles))
     for i in range(len(Articles)):
         if Articles[i]['id'] == id:
-            article = Articles[i]
-    #print(id," -- ", article, " -- " , Articles[1])        
+            article = Articles[i]    
     return render_template('article.html', article = article)
 
 @app.route('/excercises')
@@ -63,9 +62,23 @@ def project():
 def about():
     return render_template('about.html')
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            #return redirect(url_for('home'))
+            return render_template('home.html')
+        
+    return render_template('login.html', error=error)
+
+
 
 if __name__ == '__main__':
-
+    #login.run(debug=True)
     app.run(debug=True)
 
 
