cfd64fe2c8139e987d6b34bec0b5a1546501b9c2
InakiGainza <igg.git.h@gmail.com>
2023-03-21 11:50:21 +0100

    rename z files .bat

--- z_app_test_console.py
+++ z_app_test_console.py
@@ -6,15 +6,16 @@ app_test_console = Flask(__name__)
 @app_test_console.route('/')
 def run_script_cmdLine():
     
-    file = open(r'testing_execution_file.bat', 'r').read()    
+    #file = open(r'z_testing_execution_file.bat', 'r').read()    
     #file = open(r'static\\py_excercises\\20230227\\20230227-OS-Dir-Files-Example_For_Teacher.py', 'r').read()    
-    return exec(file)
+    #return exec(file)
 
-    #import os
-    #os.system('cls')
-    #os.system('cmd /c "static\\py_excercises\\20230227\\z-openCmdLine-20230227-OS-Dir-Files.py"')
+    import os
+    os.system('cls')
+    #os.system('cmd /c "z_openCmdLine-20230227-OS-Dir-Files.py"')
+    os.system('cmd /c "z_testing_execution_file.bat"')
 
-    #return "<center>upsssss i do not understand</center>"
+    return "<center>upsssss i do not understand</center>"
 
 
 if __name__ == "__main__":
--- z_testing_execution_file.bat
+++ z_testing_execution_file.bat
@@ -1,2 +1,2 @@
-python.exe 20230227-OS-Dir-Files-Example_For_Teacher.py
+python.exe z-OS-example.py
 
