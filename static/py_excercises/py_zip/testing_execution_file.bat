666df1059adc21ed42338ede02200a99855fc00c
InakiGainza <igg.git.h@gmail.com>
2023-03-25 21:29:33 +0100

    upd bitakora, testing_execution_file.bat

--- static/py_excercises/py_zip/testing_execution_file.bat
+++ static/py_excercises/py_zip/testing_execution_file.bat
@@ -2,7 +2,7 @@ python.exe 20230227-OS-Dir-Files-Example.py
 
 
 
-# python.exe 
-# import os os.system('cls') os.system('cmd /c "20230227-OS-Dir-Files-Example.py"')
+:: python.exe 
+:: import os os.system('cls') os.system('cmd /c "20230227-OS-Dir-Files-Example.py"')
 
 
