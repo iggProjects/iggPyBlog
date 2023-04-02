9f9300a8f87edcf3af9c74109e8ca9d587cbd299
InakiGainza <igg.git.h@gmail.com>
2023-04-02 23:32:20 +0200

    Upd Arith_Progres

--- static/proj_lifeGame_scripts/Arith_Progres_Sum_conc-cpu-m_proc.py
+++ static/proj_lifeGame_scripts/Arith_Progres_Sum_conc-cpu-m_proc.py
@@ -1,90 +1,94 @@
 #!/usr/bin/env python3
 
 #
-# Arithmetical Progression
+# 	Arithmetical Progression
 #   An = A1 + (n-1) * d
 #   Suma(n primeros términos) = (n * ( 2*A1 + (n-1)*d )) / 2  =  n * (A1 + An) / 2
 #
 
-import multiprocessing
-import time
-import sys
 #
 # Buscador -> https://duckduckgo.com
 # bUSQUEDA:  python multiprocessing cpu id
 # Resultado: https://stackoverflow.com/questions/10190981/get-a-unique-id-for-worker-in-python-multiprocessing-pool#10192611
 # Atencion: multiprocessing.current_process()
 #
+
+import multiprocessing
+import time
+import sys
+from os import system
+
+# CONSTANTS
+
+# Colors
+NO_COLOR = "\033[00m"
+FR_GREEN = "\033[92m"
+FR_RED   = "\033[91m"
+FR_BLUE  = "\033[34m"
+
+# param 1_ elemento inicial de la sucesión
+ELEM_INIC = 1
+# param 2: razón inicial
+RAZON_INIC = 1
+# param 3: número de términos a sumar
+N_TER = 10000 
+# param 4: número de sucesiones a calcular
+N_SUCESIONES = 100
+# param 5: número de CPU's a usar
+N_CPUS = 1
+# Número para fijar frecuencia de prints
+BASE = int(N_SUCESIONES / 10)
+
 # place thousands separator
 def place_comma(numb):
     return ("{:,}".format(numb))
 
 count = 0
-count_print = 0
 		
 # Suma de una progresión artimética de radio d y primer elemento a
 def suma_AP(d):
 # def suma_AP(d,n):
 	global count_print, count
-	resultado = sum(razon_inic + i*d for i in range(0,n_ter))
+	resultado = sum(ELEM_INIC + i*d for i in range(0,N_TER))
 	# if para imprimir pocos registros 
-	if ( (count+1) % (base) == 0):
-		count_print += 1
-		# pid=str(multiprocessing.Process.pid)
-		# name=str(multiprocessing.Process.name)
-		print(f"prints cada {base} sucesiones | print No: {place_comma(count_print)} ({count+1}) | razon: {place_comma(d)} | suma:{place_comma(resultado)}")
-
-		#	print(f"prints cada {base} sucesiones | print No: {place_comma(count)} ({count+1}) | razon: {place_comma(d)} | suma:{place_comma(resultado)} {pid} <--> {name}")
+	if ( (count) % (BASE) == 0):
+		#count_print += 1
+		pid=str(multiprocessing.Process.pid)
+		name=str(multiprocessing.Process.name)
+		print(f"\nPrint {count} | razón {d} -> Proceso: {pid} | nombre: {name}\n\t\t{FR_GREEN}SUMA DE LA PROGRESIÓN: {place_comma(resultado)}{NO_COLOR}")
 	count += 1
-	# return resultado
 
-# Ejecuta la suma para una lista de razones y primeros elementos 
-def suma(lista,n_ter,n_cpus):
-	with multiprocessing.Pool(n_cpus) as pool:
+# Ejecuta la suma para una lista de razones, todas con el mismo elemento inicial 
+def suma(lista,N_CPUS):	
+	print(f"multiprocessing pool var type: {type(multiprocessing.Pool(N_CPUS))}")
+	with multiprocessing.Pool(N_CPUS) as pool:
 		pool.map(suma_AP,lista)
-		# pool.map(suma_AP,lista,n_ter)
+		# pool.map(suma_AP,lista,N_TER)
 
 #
 # PROGRAMA
 #
 
 if __name__ == '__main__':
-
-	# Parametros
-
-	# param 1: razón inicial
-	razon_inic = int(sys.argv[1])
-	# param 2: número de términos a sumar
-	n_ter = int(sys.argv[2]) 
-	# param 3: número de sucesiones a calcular
-	n_sucesiones = int(sys.argv[3])
-	# param 4: número de CPU's a usar
-	n_cpus = int(sys.argv[4])
+	# hora inicio
+	inicio = time.time()
+	system('cls')
 
 	# Lista de n razones para las n sucesiones
-	lista_razones = [razon_inic + 2*x for x in range(0, n_sucesiones)]
-
-	# Intervalo para Prints  
-	base = int(n_sucesiones / 10)
-
-	inicio = time.time()
+	lista_razones = [(RAZON_INIC + 2*x) for x in range(0, N_SUCESIONES)]
 
 	# CALL multiprocessing function
-	suma(lista_razones,n_ter,n_cpus)
-
-	duracion = time.time() - inicio
-
-	# cálculo razón de la sucesión final (para mostrarla)
-	razon_fin = lista_razones[n_sucesiones-1]
+	suma(lista_razones,N_CPUS)
 
 	# cálculo aprox de operaciones aritméticas
-	oper_arit = 2*n_sucesiones*n_ter
-
-	# print(f"lista de razones: {lista_razones} ")
-	print(f"-----------BALANCE--------------")
-	print(f"Nro prints teóricos: {place_comma(int(n_sucesiones/base))} | nro prints: {count_print}")
-	print(f"razon 1ra Suc: {razon_inic} | razon ult Suc: {place_comma(razon_fin)} | nro términos a sumar: {n_ter} | nro sucesiones: {place_comma(n_sucesiones)}")
-	print(f"numb PCU: {n_cpus} | estimado operac aritmeticas: {place_comma(oper_arit)}")
+	oper_arit = 2 * N_SUCESIONES * N_TER
 
 	duracion = time.time() - inicio
-	print(f"Duracion {duracion} segundos")
+	dur_5_dec = "{:.5f}".format(duracion)
+
+	print(f"\n{FR_RED}-----------BALANCE--------------{NO_COLOR}\n")		
+
+	print(f"\n{place_comma(N_SUCESIONES)} Sucesiones | Elem Inicial {ELEM_INIC} | No Términos {place_comma(N_TER)}\n")
+	print(f"{FR_GREEN}======= Lista razones para las sucesiones ======={NO_COLOR}\n{lista_razones}")
+	print(f"\nNo CPU's: {N_CPUS} | Estimado operac aritmeticas: {place_comma(oper_arit)} | Nro prints teóricos: {place_comma(int(N_SUCESIONES/BASE))}")
+	print(f"\n{FR_GREEN}Duracion proceso sin incluir tiempo impresión BALANCE: {dur_5_dec} segundos{NO_COLOR}\n")
