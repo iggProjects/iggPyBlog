9eec9c2431ae155e8a27f2697902339747238947
InakiGainza <igg.git.h@gmail.com>
2023-04-17 12:51:44 +0200

    upd article data and html

--- article_data.py
+++ article_data.py
@@ -2,14 +2,92 @@ def Articles():
     articles = [       
         {
             'id': 1, 
+            'title': 'Stackoverflow Survey 2022',            
+            'author': 'survey.stackoverflow 2022',
+            'date_created': '31-12-2022',
+            'section':'',
+            'summary': 'Visual Studio Code remains the preferred IDE across all developers. PyCharm is used more by people learning to code (26% vs 16%) while Vim is used more by Professional Developers (24% vs 16%).',
+            'body': '',
+            'page_url': 'https://survey.stackoverflow.co/2022/'
+        },
+        {
+            'id': 2, 
+            'title': 'Programming, scripting, and markup languages.',            
+            'author': 'survey.stackoverflow 2022',
+            'date_created': '31-12-2022',
+            'section':'',
+            'summary': 'Programming, scripting, and markup languages. 2022 marks JavaScript’s tenth year in a row as the most commonly used programming language. But, it’s a different picture for those learning to code. HTML/CSS, Javascript and Python are almost tied as the most popular languages for people learning to code. People learning to code are more likely than Professional Developers to report using Python (58% vs 44%), C++ (35% vs 20%), and C (32% vs 17%).',
+            'body': '',
+            'page_url': 'https://survey.stackoverflow.co/2022/#section-most-popular-technologies-programming-scripting-and-markup-languages'
+        },
+
+        {
+            'id': 3, 
+            'title': 'Why is Python Growing So Quickly?',            
+            'author': 'Devid Robinson',
+            'date_created': '17-04-2017',
+            'section':'',
+            'summary': 'Why is Python Growing So Quickly? We recently showed that, based on Stack Overflow question visits, Python has a claim to being the fastest-growing major programming language, and that it has become the most visited tag on Stack Overflow within high-income countries.',
+            'body': '',
+            'page_url': 'https://stackoverflow.blog/2017/09/14/python-growing-quickly/'
+        },
+
+
+        {
+            'id': 4, 
+            'title': 'Stack Overflow confirms Python’s popularity...',            
+            'author': 'Richard Gall',
+            'date_created': '10-04-2019',
+            'section':'',
+            'summary': 'Stack Overflow survey data further confirms Python’s popularity as it moves above Java in the most used programming language list. Three reasons for Python\'s growth: 1.- in Python’s easy to learn; 2.- The growth of data science and machine learning; 3.- Python is a flexible language.',
+            'body': '',
+            'page_url': 'https://hub.packtpub.com/stack-overflow-survey-data-further-confirms-pythons-popularity-as-it-moves-above-java-in-the-most-used-programming-language-list/'
+        },
+
+        {
+            'id': 5, 
+            'title': 'Stack Overflow survey data further confirms Python’s popularity...',            
+            'author': 'Matthew Harper',
+            'date_created': '17-10-2022',
+            'section':'',
+            'summary': 'After its release in 1990 and experiencing a huge boom in popularity in the 2010s, Python has grown into one of the most widespread programming languages in the world. Many universities have begun teaching Python in their introduction to programming courses, and it’s a common first programming language for individuals learning how to code. Despite its popularity, Python is a controversial topic among software developers.',
+            'body': '',
+            'page_url': 'https://spin.atomicobject.com/2022/10/17/python-development/'
+        },
+
+        {
+            'id': 6, 
+            'title': 'Top 8 Reasons for Using TypeScript / JavaScript (Node.js) instead of Python for z/OS',            
+            'author': 'Dan Kelosky',
+            'date_created': '29-11-2022',
+            'section':'',
+            'summary': 'GitHub source statistics say JavaScript is the #1 most used language.',
+            'body': '',
+            'page_url': 'https://dkelosky.medium.com/top-8-reasons-for-using-typescript-javascript-node-js-instead-of-python-for-z-os-eb3e0b3f3cdc'
+        },
+
+        {
+            'id': 7, 
+            'title': 'Why Python is not the programming language of the future',            
+            'author': 'Ari Youry',
+            'date_created': '31-05-2020',
+            'section':'',
+            'summary': '',
+            'body': '',
+            'page_url': 'https://towardsdatascience.com/why-python-is-not-the-programming-language-of-the-future-30ddc5339b66'
+        },
+
+
+        {
+            'id': 10, 
             'title': 'Google Colaboratory',            
             'author': 'Iñaki Gainzarain',
             'date_created': '26-01-2022',
-            'pages':'',
-            'summary': 'El Módulo de Caja Flexible, comúnmente llamado flexbox, fue diseñado como un modelo unidimensional de layout, y como un método que pueda ayudar a distribuir el espacio entre los ítems de una interfaz y mejorar las capacidades de alineación. Este artículo hace un repaso de las principales características de flexbox, las que exploraremos con mayor detalle en el resto de estas guías.',
-            'body': 'Esta infracción de tráfico se considera una falta grave o muy grave, en función de cómo se produzca. Por ejemplo, si en una carretera con el límite a 60 km/h se circula a una velocidad superior a 61 y menor a 90 se trata de una sanción administrativa, sin retirada de puntos. Sin embargo, si la velocidad llega a ser de 131 km/h o más, se considera una sanción grave y la multa asciende a 600 euros con la pérdida de seis puntos del carnet de conducir. En este contexto, el artículo 379 del Código Penal establece que se incurre en un delito contra la seguridad del tráfico si se supera la velocidad permitida en más de 60 km/h en ciudad o en más de 80 en zona interurbana. Esta infracción de tráfico se considera una falta grave o muy grave, en función de cómo se produzca. Por ejemplo, si en una carretera con el límite a 60 km/h se circula a una velocidad superior a 61 y menor a 90 se trata de una sanción administrativa, sin retirada de puntos. Sin embargo, si la velocidad llega a ser de 131 km/h o más, se considera una sanción grave y la multa asciende a 600 euros con la pérdida de seis puntos del carnet de conducir. En este contexto, el artículo 379 del Código Penal establece que se incurre en un delito contra la seguridad del tráfico si se supera la velocidad permitida en más de 60 km/h en ciudad o en más de 80 en zona interurbana. Esta infracción de tráfico se considera una falta grave o muy grave, en función de cómo se produzca. Por ejemplo, si en una carretera con el límite a 60 km/h se circula a una velocidad superior a 61 y menor a 90 se trata de una sanción administrativa, sin retirada de puntos. Sin embargo, si la velocidad llega a ser de 131 km/h o más, se considera una sanción grave y la multa asciende a 600 euros con la pérdida de seis puntos del carnet de conducir. En este contexto, el artículo 379 del Código Penal establece que se incurre en un delito contra la seguridad del tráfico si se supera la velocidad permitida en más de 60 km/h en ciudad o en más de 80 en zona interurbana. Esta infracción de tráfico se considera una falta grave o muy grave, en función de cómo se produzca. Por ejemplo, si en una carretera con el límite a 60 km/h se circula a una velocidad superior a 61 y menor a 90 se trata de una sanción administrativa, sin retirada de puntos. Sin embargo, si la velocidad llega a ser de 131 km/h o más, se considera una sanción grave y la multa asciende a 600 euros con la pérdida de seis puntos del carnet de conducir. En este contexto, el artículo 379 del Código Penal establece que se incurre en un delito contra la seguridad del tráfico si se supera la velocidad permitida en más de 60 km/h en ciudad o en más de 80 en zona interurbana. Esta infracción de tráfico se considera una falta grave o muy grave, en función de cómo se produzca. Por ejemplo, si en una carretera con el límite a 60 km/h se circula a una velocidad superior a 61 y menor a 90 se trata de una sanción administrativa, sin retirada de puntos. Sin embargo, si la velocidad llega a ser de 131 km/h o más, se considera una sanción grave y la multa asciende a 600 euros con la pérdida de seis puntos del carnet de conducir. En este contexto, el artículo 379 del Código Penal establece que se incurre en un delito contra la seguridad del tráfico si se supera la velocidad permitida en más de 60 km/h en ciudad o en más de 80 en zona interurbana.Esta infracción de tráfico se considera una falta grave o muy grave, en función de cómo se produzca. Por ejemplo, si en una carretera con el límite a 60 km/h se circula a una velocidad superior a 61 y menor a 90 se trata de una sanción administrativa, sin retirada de puntos. Sin embargo, si la velocidad llega a ser de 131 km/h o más, se considera una sanción grave y la multa asciende a 600 euros con la pérdida de seis puntos del carnet de conducir. En este contexto, el artículo 379 del Código Penal establece que se incurre en un delito contra la seguridad del tráfico si se supera la velocidad permitida en más de 60 km/h en ciudad o en más de 80 en zona interurbana.',
-            'zip_url': ''
+            'section':'',
+            'summary': '',
+            'page_url': ''
         },
+
 
     ]
 
