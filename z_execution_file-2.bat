ef46922caef2b790bdd235b636ad0e2d1854eda2
InakiGainza <igg.git.h@gmail.com>
2023-04-20 11:55:25 +0200

    remove and rename for some files

--- z_execution_file-2.bat
+++
@@ -1,3 +0,0 @@
-system('cmd /c "static\py_excercises\20230227-OS-Examples\20230227-OS-Dir-Files-Example.py"')
-
-
--- z_execution_file.bat
+++
@@ -1,2 +0,0 @@
-python.exe static/py_excercises/20230227-OS-Examples/20230227-OS-Dir-Files-Example.py
-
