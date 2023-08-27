dcd5e7b202bfd0b055c7b070664daf3411846b7f
InakiGainza <igg.git.h@gmail.com>
2023-08-27 20:44:35 +0200

    UPD server_messages, z-2, z-3 messages scripts

--- static/proj_enigmaGame_scripts/z-2_AlphabGenerate_25_WithOneRotor1.py
+++ static/proj_enigmaGame_scripts/z-2_AlphabGenerate_25_WithOneRotor1.py
@@ -10,24 +10,13 @@ FR_YELL  = "\033[93m"
 FR_BLUE  = "\033[94m"
 FR_MAG   = "\033[95m"
 
-def alphab_jump(alph,k,msg):
-  # jump valur to rotate list
-  jump = -k  
-  alphab_j = alph[jump:] + alph[:jump]
-  """
-  # print first and second terms
-  print(f"{FR_GREEN}first and second terms{NO_COLOR}")
-  print(f"{FR_YELL}alphab[ {jump} : ]{NO_COLOR}\n{str(alphab[jump:])}" )
-  print(f"{FR_YELL}alphab[ : {jump} ]{NO_COLOR}\n{str(alphab[:jump])}" )  
-  """
-  #print(f"{FR_GREEN}{msg}: {NO_COLOR}\n{str(alphab_j)}" )
-  return alphab_j
-
+def alphab_jump(alph,k):
+  # jump value -k to rotate list    
+  return alph[-k:] + alph[:-k]
 
 if __name__ == "__main__":
 
   print("print empty line")
-  alphab_1 = ""
 
   # create list of alphabet
   string.ascii_lowercase 
@@ -35,16 +24,13 @@ if __name__ == "__main__":
   alphab = list(string.ascii_lowercase)
   print(f"{FR_YELL}\nORIGINAL ALPHABET LIST{NO_COLOR} (length: {len(alphab)})\n{alphab}\n\n")
   #print("print empty line")
-
-  jump=1
-  msg = "Rotate + " + str(jump)
-  alphab_list = []
-  for i in range(27):    
-    jump = i
-    print(jump)
-    #new_alphab = 
-    alphab_list.append(alphab_jump(alphab,jump, msg))
-    print(alphab_list[i])
+  
+  # list of all alphabets with rotation by 1 to right position
+  alphab_list = []  
+  for jump in range(27):        
+    print(jump)     
+    alphab_list.append(alphab_jump(alphab,jump))
+    print(alphab_list[jump])
 
    
   
