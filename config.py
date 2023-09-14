d480c733d4760f23c1b73f9e885a057c8d58284c
InakiGainza <igg.git.h@gmail.com>
2023-09-14 21:39:32 +0200

    UPD config.py, flask_app

--- config.py
+++ config.py
@@ -22,12 +22,21 @@ import logging
 # My Own Colors
 from MyColors import *
 
+# base dir in server
 basedir = os.path.abspath(os.path.dirname(__file__))
-#print(f"{FR_GREEN}............ basedir ===> {basedir}{NO_COLOR}")
+print(f"{FR_GREEN}............ basedir ===> {basedir}{NO_COLOR}")
 
+# operating system in server
 opSys = platform.system()
-#print(f"{FR_YELL}............ OS ===> {opSys}{NO_COLOR}")
+print(f"{FR_YELL}............ OS ===> {opSys}{NO_COLOR}")
 
+# machine specifications
+myMachine = platform.uname()
+#print(f"==== host name: {myMachine}")
+
+#print(f"globals: {type(globals())}\n{globals()}")
+
+# log messages
 logging.basicConfig(filename=basedir + "/static/logFiles/server_messages.log", 
                     encoding='utf-8', level=logging.DEBUG, format="%(asctime)-15s %(levelname)-8s %(funcName)s %(message)s")
 logging.captureWarnings(True)
