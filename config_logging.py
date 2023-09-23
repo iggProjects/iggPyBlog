8d08b4e114647a67558fec522690a2b91ab859f8
InakiGainza <igg.git.h@gmail.com>
2023-09-23 21:30:51 +0200

    UPD config_logging

--- config_logging.py
+++ config_logging.py
@@ -4,7 +4,6 @@ import os, logging
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # error handling 
-import logging
 log_file_path = basedir + "/static/logFiles/server_messages.txt"
 logging.basicConfig(filename=log_file_path, 
                 encoding='utf-8', level=logging.DEBUG, format="%(asctime)-15s %(levelname)-8s %(funcName)s %(message)s")
