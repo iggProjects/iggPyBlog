6e5638d7826564473d39e140b6e953d4a6880b69
InakiGainza <igg.git.h@gmail.com>
2023-09-24 18:55:03 +0200

    UPD config, 0-prototype-colors1

--- config.py
+++ config.py
@@ -23,16 +23,15 @@ from static.include.MyColors import *
 
 # base dir in server
 basedir = os.path.abspath(os.path.dirname(__file__))
-print(f"{FR_GREEN}==== basedir: {basedir}{NO_COLOR}")
+#print(f"{FR_GREEN}==== basedir:{NO_COLOR}\n\t{basedir}")
 
 # operating system in server
 opSys = platform.system()
-print(f"{FR_YELL}==== OS: {opSys}{NO_COLOR}")
+#print(f"{FR_YELL}==== OS:{NO_COLOR}\n\t{opSys}{NO_COLOR}")
 
 # machine specifications
 myMachine = platform.uname()
-print(f"{FR_MAG}==== machine specif:{NO_COLOR}")
-print(f"\t{myMachine}")
+#print(f"{FR_BLUE}==== machine specif:{NO_COLOR}\n\t{myMachine}")
 
 app = Flask(__name__)
 app.secret_key = 'HI TARZAN'
